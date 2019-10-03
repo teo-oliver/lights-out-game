e633d84098b8daa77f4bf5f2c60870bd4df830fe
Teo Oliver <teo.liz.oli@gmail.com>
2019-10-03 17:26:39 +0100

    add react-transition-group

--- src/App.js
+++ src/App.js
@@ -3,16 +3,43 @@ import Board from './Board';
 import Home from './Home';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 /** Simple app that just shows the LightsOut game. */
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/board" component={Board} />
-        </Switch>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} classNames="fade" timeout={900}>
+                <Switch location={location}>
+                  <Route
+                    exact
+                    path="/"
+                    render={() => (
+                      <div className="page">
+                        <Home />
+                      </div>
+                    )}
+                  />
+
+                  <Route
+                    exact
+                    path="/board"
+                    render={() => (
+                      <div className="page">
+                        <Board />
+                      </div>
+                    )}
+                  />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </div>
     );
   }
